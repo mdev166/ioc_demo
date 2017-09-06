9bd62520634535e3cc3622cf35b841ffd4762112
mdev166 <mdev166@users.noreply.github.com>
2017-09-05 20:13:01 -0700

    Add web test project.
    Add web test project.

---
+++ IoC.Web.Test/HomeControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using IoC.Web.Controllers;
+using IoC.Web.Models;
+using IoC.Web.Services;
+using Moq;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Xunit;
+
+namespace IoC.Web.Test
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexSucceedsTest()
+        {
+            var service = new Mock<IEmployeeService>();
+            var logger = new Mock<ILoggerService>();
+            service.Setup(svc => svc.GetEmployees())
+                .Returns(new List<Employee>(new List<Employee> { new Employee() {  FirstName = "Bob" } }));
+            var controller = new HomeController(logger.Object, service.Object);
+
+            var result = controller.Index() as ViewResult;
+            Assert.NotNull(result);
+            var model = result.Model as IList<Employee>;
+            Assert.NotNull(model);
+            Assert.Equal(1, model.Count);
+            Assert.Equal("Bob", model[0].FirstName);
+        }
+        [Fact]
+        public void AboutSucceedsTest()
+        {
+            var service = new Mock<IEmployeeService>();
+            var logger = new Mock<ILoggerService>();
+            service.Setup(svc => svc.GetEmployees())
+                .Returns(new List<Employee>(new List<Employee> { new Employee() { FirstName = "Bob" } }));
+            var controller = new HomeController(logger.Object, service.Object);
+
+            var result = controller.About() as ViewResult;
+            Assert.NotNull(result);
+        }
+    }
+}
---
+++ IoC.Web.Test/SalesControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using IoC.Web.Controllers;
+using IoC.Web.Models;
+using IoC.Web.Services;
+using Moq;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Xunit;
+
+namespace IoC.Web.Test
+{
+    public class SalesControllerTests
+    {
+        [Fact]
+        public void IndexSucceedsTest()
+        {
+            var logger = new Mock<ILoggerService>();
+            var service = new Mock<ISalesService>();
+            var empService = new Mock<IEmployeeService>();
+            service.Setup(svc => svc.GetSalesForEmployee(10))
+                .Returns(new List<Sale>(new List<Sale> { new Sale() {  Amount = 1200.00M } }));
+            empService.Setup(svc => svc.GetEmployee(10))
+                .Returns(new Employee() { Id = 10, FirstName = "Sally", LastName = "Test" });
+            var controller = new SalesController(logger.Object, service.Object, empService.Object);
+
+            var result = controller.Index(10) as ViewResult;
+            Assert.NotNull(result);
+            var model = result.Model as SalesViewModel;
+            Assert.NotNull(model);
+            Assert.NotNull(model.SalesPerson);
+            Assert.Equal("Sally", model.SalesPerson.FirstName);
+            Assert.Equal(1, model.Sales.Count);
+        }
+    }
+}
